3e9c6aa38c44c81274a05fb7d12e63744d9102d4
Akash Rana <akashrana.india@gmail.com>
2024-10-19 21:30:55 +0530

    spaceship operator functionality

---
+++ spaceship_operator.py
@@ -0,0 +1,10 @@
+# x <=> y
+
+# <=> operator
+# if x is greater than y result will 1
+# if x is less than y result will -1
+# if x is equal to y result will 0
+
+x, y = 10,2
+result: int = (x>y) - (x<y) 
+print(result)
